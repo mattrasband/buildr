7072ae7092e2adc75c40853c92938115419eac83
Matt Rasband <matt.rasband@gmail.com>
2016-11-12 19:19:53 -0500

    Converted logic to be a context manager class
    * Wanted to ensure that resources are always reaped when used, the context manager allows for usage in other scenarios without expecting them to run cleanup.
    * Add some docstrings

--- buildr.py
+++ buildr.py
@@ -26,6 +26,7 @@ class BuildFailure(Exception):
 
 
 class ManifestV1:
+    """Required properties for a version 1 manifest"""
     def __init__(self, manifest_def):
         self._def = manifest_def
 
@@ -62,14 +63,16 @@ class ManifestV1:
         return self._env
 
     def __getitem__(self, value):
+        """Access stage definitions as dict keys"""
         return self._def.get(value)
 
 
 def parse_args():
+    """Parse the command line arguments"""
     parser = argparse.ArgumentParser(prog='buildr',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)  # noqa
     parser.add_argument('--path', help='Local directory path', type=str,
-                        default=True)
+                        default='.')
     parser.add_argument('--docker-sock', help='Docker sock', type=str,
                         default='unix://var/run/docker.sock')
 
@@ -77,6 +80,11 @@ def parse_args():
 
 
 def load_manifest(project_dir: pathlib.Path) -> ManifestV1:
+    """Load the manifest from the project directory,
+    erroring on the manifest not being found or the
+    project directory not existing.
+    :param project_dir: Where the project is on disk
+    :returns ManifestV1: The parsed manifest"""
     if not project_dir.exists():
         sys.exit('Project directory does not exist')
     logger.debug('Searching for manifest in %s', project_dir)
@@ -94,90 +102,126 @@ def load_manifest(project_dir: pathlib.Path) -> ManifestV1:
         sys.exit('Illegal manifest version')
 
 
+class Buildr:
+    def __init__(self, project_dir: pathlib.Path, *,
+                 base_url='unix://var/run/docker.sock', image='docker',
+                 env=None):
+        self.project_dir = project_dir
+        self.base_url = base_url
+        self.image = image
+        self.env = env
+        if env is None:
+            self.env = []
+
+        self.cli = None
+        self.container_id = None
+        self._cm = False
+
+    def __enter__(self):
+        self.cli = Client(base_url=self.base_url)
+        self.container_id = self._create_container()
+        self._start_container()
+        self._cm = True
+
+        return self
+
+    def __exit__(self, *args):
+        self._close_container()
+        self._cm = False
+
+    def execute(self, command: str, writer=sys.stdout.write) -> int:
+        """Execute a build command.
+        :param command: Command to execute in the shell
+        :returns: Exit code"""
+        if not self._cm:
+            raise ValueError('Buildr must be run as a context manager to'
+                             ' ensure all resources are reaped on exit.')
+
+        # You can emulate this with:
+        #   docker exec <container_name> <script>
+        exec_ = self.cli.exec_create(self.container_id, command)
+        exec_id = exec_['Id']
+
+        for chunk in self.cli.exec_start(exec_id, stream=True):
+            try:
+                writer(chunk.decode())
+            except UnicodeDecodeError:
+                pass
+
+        result = self.cli.exec_inspect(exec_id)
+        return result['ExitCode']
+
+    def _create_container(self):
+        """Creates the build runner container"""
+        # For debug, you can emulate this with:
+        #   docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}":/app \
+        #   --workdir /app -e <your envvars> <manifest.image> sh
+        container = self.cli.create_container(image=self.image,
+                                         command='sh',
+                                         detach=True,
+                                         environment=self.env,
+                                         stdin_open=True,
+                                         working_dir='/app',
+                                         volumes=[
+                                             '/app',
+                                             '/var/run/docker.sock',
+                                             # '/root/.docker/config.json',  # TODO: remove
+                                         ],
+                                         host_config=self.cli.create_host_config(binds=[
+                                             '{}:/app'.format(self.project_dir.absolute()),  # noqa
+                                             '/var/run/docker.sock:/var/run/docker.sock',  # noqa
+                                             # TODO: this is temporary, it really should
+                                             # probably just be expected to be baked into
+                                             # the build image.
+                                             # '/root/.docker/config.json:/root/.docker/config.json'  # noqa
+                                         ]))
+        return container['Id']
+
+    def _start_container(self):
+        """Start the container"""
+        self.cli.start(self.container_id)
+
+    def _close_container(self):
+        """Close, shutdown, and remove the container"""
+        self.cli.kill(self.container_id)
+        self.cli.remove_container(self.container_id)
+
+
 def run_manifest(manifest: ManifestV1, target_dir: pathlib.Path, *,
-                 docker_sock='unix://var/run/docker.sock'):
-    logger.debug('Project directory: %s', target_dir)
+                 docker_sock='unix://var/run/docker.sock',
+                 progress_writer=sys.stdout,
+                 project_meta=None):
+    """Run the manifest stages.
 
-    stages = manifest.stages
-    image = manifest.image
-    env_vars = manifest.env
-
-    logger.debug('Connecting to docker sock: %s', docker_sock)
-    cli = Client(base_url=docker_sock)
-
-    # For debug, you can emulate this with:
-    #   docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v "${PWD}":/app \
-    #   --workdir /app -e <your envvars> <manifest.image> sh
-    container = cli.create_container(image=image,
-                                     command='sh',
-                                     detach=True,
-                                     environment=env_vars,
-                                     stdin_open=True,
-                                     working_dir='/app',
-                                     volumes=[
-                                         '/app',
-                                         '/var/run/docker.sock',
-                                         '/root/.docker/config.json',
-                                     ],
-                                     host_config=cli.create_host_config(binds=[
-                                         '{}:/app'.format(target_dir.absolute()),  # noqa
-                                         '/var/run/docker.sock:/var/run/docker.sock',  # noqa
-                                         # TODO: this is per user, not sure how it works
-                                         # if we run in the container as a diff user
-                                         '/root/.docker/config.json:/root/.docker/config.json'
-                                     ]))
-
-    container_id = container['Id']
-    try:
-        logger.debug('Created container: %s', container_id)
-        cli.start(container_id)
-        logger.debug('Started container')
-
-        logger.debug('Running stages: %s', stages)
-        for stage_name in stages:
-            # TODO: precondition check once we decide how to do that.
-            logger.info('Executing stage: %s', stage_name)
-            stage = manifest[stage_name]
-            for script in stage.get('script', []):
-                logger.debug('Executing script "%s"', script)
-                # TODO: Add user, by default run as non-root unless manifest suggests we need it
-                # You can emulate this with:
-                #   docker exec <container_name> <script>
-                exec_ = cli.exec_create(container_id, script)
-                exec_id = exec_['Id']
-
-                for chunk in cli.exec_start(exec_id, stream=True):
-                    try:
-                        sys.stdout.write(chunk.decode())
-                    except UnicodeDecodeError:
-                        pass
-
-                result = cli.exec_inspect(exec_id)
-                ec = result['ExitCode']
-                if ec != 0:
-                    if stage_name == 'prepare':
-                        logger.error('Command exited with error, unable to prepare the environment.')
-                        raise BuildError('Prepare failed, unable to set up the environment.')
-                    else:
-                        logger.error('Command exited with error, build failed.')
-                        raise BuildFailure('Stage failed.')
-
-            logger.info('Stage "%s" passed', stage_name)
-    except KeyboardInterrupt:
-        print('Exiting gracefully')
-    except Exception as e:
-        logger.error('Error running manifest:', e)
-    finally:
-        print('Killing container...')
-        cli.kill(container_id)
-        print('Removing container...')
-        cli.remove_container(container_id)
-        print('Finished.')
+    :param manifest: The manifest to run
+    :param target_dir: Which directory the source is in that the manifest
+                       targets.
+    :param docker_sock: Location of the docker engine socket.
+    :param progress_writer: Stream to write echoed stdout from the
+                            build container to, defaults to stdout."""
+    with Buildr(target_dir, base_url=docker_sock, image=manifest.image,
+                env=manifest.env) as buildr:
+        try:
+            for stage_name in manifest.stages:
+                stage = manifest[stage_name]
+                for script in stage.get('script', []):
+                    rc = buildr.execute(script)
+                    if rc != 0:
+                        if stage_name == 'prepare':
+                            logger.error('Command exited with error, unable to '
+                                         'prepare the environment.')
+                            raise BuildError('Prepare failed, unable to set up '
+                                             'the environment.')
+                        else:
+                            logger.error('Command exited with error, build '
+                                         'failed.')
+                            raise BuildFailure('Stage failed.')
+        except KeyboardInterrupt:
+            print('Run cancelled, exiting')
 
 
 def main():
     args = parse_args()
-
     target_dir = pathlib.Path(args.path)
     manifest = load_manifest(target_dir)
     run_manifest(manifest, target_dir, docker_sock=args.docker_sock)
